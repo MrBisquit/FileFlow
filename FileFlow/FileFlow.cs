376257705790e517295c2781f8fe77a884bacfe1
MrBisquit <william@wtdawson.info>
2023-02-25 15:37:37 +0000

    Add project files.

---
+++ FileFlow/FileFlow.cs
@@ -0,0 +1,42 @@
+﻿namespace FileFlow
+{
+    public class File
+    {
+        public FileInfo GetFile(string path)
+        {
+            return new FileInfo(path);
+        }
+    }
+    public class Directory
+    {
+        public class FileSystemListener
+        {
+            FileSystemWatcher watcher;
+            public FileSystemListener(DirectoryInfo directory)
+            {
+                watcher = new FileSystemWatcher(directory.FullName);
+                watcher.EnableRaisingEvents = true;
+            }
+            public void AddFileCreatedListener(FileSystemEventHandler action)
+            {
+                watcher.Created += action;
+            }
+            public void AddFileChangedListener(FileSystemEventHandler action)
+            {
+                watcher.Changed += action;
+            }
+            public void AddFileRenamedListener(RenamedEventHandler action)
+            {
+                watcher.Renamed += action;
+            }
+            public void AddFileDeletedListener(FileSystemEventHandler action)
+            {
+                watcher.Deleted += action;
+            }
+            public void Destroy()
+            {
+                watcher.Dispose();
+            }
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fsl = new FileFlow.Directory.FileSystemListener(new DirectoryInfo(Console.ReadLine()));
+            Console.Clear();
+                Console.WriteLine("| Type    | File name");
+            fsl.AddFileCreatedListener((sender, e) =>
+            {
+                Console.WriteLine("| Created | " + e.Name);
+            });
+            fsl.AddFileChangedListener((sender, e) =>
+            {
+                Console.WriteLine("| Changed | " + e.Name);
+            });
+            fsl.AddFileDeletedListener((sender, e) =>
+            {
+                Console.WriteLine("| Deleted | " + e.Name);
+            });
+            fsl.AddFileRenamedListener((sender, e) => {
+                Console.WriteLine("| Renamed | " + e.Name);
+            });
+            Console.ReadLine();
+        }
+    }
+}
